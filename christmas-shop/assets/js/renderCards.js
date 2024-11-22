2a28539653c7aa41efc60a06346cd061ccabd604
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-11-22 20:45:28 +0700

    feat: add render cards script

---
+++ christmas-shop/assets/js/renderCards.js
@@ -0,0 +1,75 @@
+import giftsData from "../gifts.json";
+
+function createDOMElement(option) {
+    let {
+        tagName = 'div',
+        appendParent = null,
+        classList = '',
+        textContent = '',
+        attributes = {},
+
+    } = option;
+    const element = document.createElement(tagName);
+    if (appendParent) appendParent.append(element);
+    if (classList) {
+        classList = classList.split(" ");
+        classList.forEach(newClass => {
+                element.classList.add(newClass)
+            }
+        );
+    }
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    return element;
+}
+
+function createCard(data) {
+    const cardCategory = {
+        'For Work': 'card-work',
+        'For Health': 'card-health',
+        'For Harmony': 'card-harmony',
+    }
+    const cardItem = createDOMElement({
+        tagName: 'li',
+        classList: `card-item flex ${cardCategory[data.category]}`
+    });
+    const cardText = createDOMElement({
+        tagName: 'div',
+        classList: `card-text`,
+        appendParent: cardItem
+    });
+    createDOMElement({
+        tagName: 'p',
+        classList: 'header_4',
+        textContent: data.category,
+        appendParent: cardText
+    });
+    createDOMElement({
+        tagName: 'p',
+        classList: 'header_3',
+        textContent: data.name,
+        appendParent: cardText
+    });
+    return cardItem;
+}
+
+function getRandomArr(num, maxIndex) {
+    let result = new Set;
+    while (result.size < num) {
+        result.add(Math.floor(Math.random() * maxIndex));
+    }
+    return [...result];
+}
+
+export function renderCards(num, maxIndex) {
+    const cardsList = document.querySelector('.card-list');
+    const cardsArr = getRandomArr(num, maxIndex);
+    // console.log(cardsArr);
+    cardsArr.forEach((index) => {
+        cardsList.appendChild(createCard(giftsData[index]));
+    })
+}
--- christmas-shop/main.js
+++ christmas-shop/main.js
@@ -1,7 +1,17 @@
 import {burgerMenu} from "./assets/js/burger-menu.js";
 import {slider} from "./assets/js/slider.js";
 import {timer} from "./assets/js/timer.js";
+import {renderCards} from "./assets/js/renderCards.js";
+
+
 
 burgerMenu()
 slider()
 timer()
+
+
+
+
+renderCards(4, 36)
+
+
