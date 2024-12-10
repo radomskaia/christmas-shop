9ed43515c1677d18eda8797f684817a924e906a9
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-12-10 11:20:17 +0700

    refactor: change entry point

--- christmas-shop/main.js
+++ christmas-shop/main.js
@@ -1,13 +1,3 @@
-import {burgerMenu} from "./assets/js/burger-menu.js";
-import {slider} from "./assets/js/slider.js";
-import {timer} from "./assets/js/timer.js";
-import {renderCards} from "./assets/js/renderCards.js";
-import {modal} from "./assets/js/modal.js";
+import {init} from "./assets/js/app.js";
 
-
-
-burgerMenu()
-slider()
-timer()
-renderCards('all', 'home')
-modal()
+init('home')
